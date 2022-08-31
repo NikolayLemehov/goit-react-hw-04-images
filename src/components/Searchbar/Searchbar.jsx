961b89cf02816029e00807905b27e1042eb7cbeb
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-31 10:41:59 +0300

    to function Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import s from './Searchbar.module.css'
+import s from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  handleFormSubmit = (e) => {
+function Searchbar({ onSubmit }) {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    this.props.onSubmit(e.target.elements['search'].value.toLowerCase().trim())
-  }
+    onSubmit(e.target.elements['search'].value.toLowerCase().trim());
+  };
 
-  render() {
-    return (
-      <header className={s.container}>
-        <form className={s.form} onSubmit={this.handleFormSubmit}>
-          <button type="submit" className={s.btn}>
-            <span className={s.btnLabel}>Search</span>
-          </button>
+  return (
+    <header className={s.container}>
+      <form className={s.form} onSubmit={handleFormSubmit}>
+        <button type='submit' className={s.btn}>
+          <span className={s.btnLabel}>Search</span>
+        </button>
 
-          <input
-            className={s.input}
-            type="text"
-            name='search'
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={s.input}
+          type='text'
+          name='search'
+          autoComplete='off'
+          autoFocus
+          placeholder='Search images and photos'
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
