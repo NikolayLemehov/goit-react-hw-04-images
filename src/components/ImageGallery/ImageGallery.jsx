733367036682784bb1190d05928cf8d27d43bf2b
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-31 10:38:34 +0300

    to function ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,34 +1,25 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
-class ImageGallery extends PureComponent {
-  state = {
-    visibilityBtn: false,
-  };
-
-  render() {
-    const { images, onClickImg } = this.props;
-    // console.log(images.map(it => it.id))
-
-    return (
-      <ul className={s.container}>
-        {images.map(img => {
-          const { frontId, id, webformatURL, largeImageURL, tags } = img;
-          return (
-            <ImageGalleryItem
-              key={frontId}
-              id={id}
-              tags={tags}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-              onClickImg={onClickImg}
-            />);
-        })}
-      </ul>
-    );
-  }
+function ImageGallery({ images, onClickImg }) {
+  return (
+    <ul className={s.container}>
+      {images.map(img => {
+        const { frontId, id, webformatURL, largeImageURL, tags } = img;
+        return (
+          <ImageGalleryItem
+            key={frontId}
+            id={id}
+            tags={tags}
+            webformatURL={webformatURL}
+            largeImageURL={largeImageURL}
+            onClickImg={onClickImg}
+          />);
+      })}
+    </ul>
+  );
 }
 
 ImageGallery.propTypes = {
