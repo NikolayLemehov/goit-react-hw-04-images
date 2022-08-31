09872b73f9bdf3b10ce89feeb1629108c5e6c2f1
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-31 11:08:08 +0300

    fixes eslint

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types';
 import s from './Modal.module.css'
 
 function Modal({onKeyDownEsc, src, alt}) {
-  const onKeyDown = (e) => {
-    if (e.key !== 'Escape') return;
-    closeModal()
-  }
-
-  const closeModal = () => onKeyDownEsc()
-
   useEffect(() => {
+    const onKeyDown = (e) => {
+      if (e.key !== 'Escape') return;
+      onKeyDownEsc()
+    }
     window.addEventListener('keydown', onKeyDown)
     return () => {
       window.removeEventListener('keydown', onKeyDown)
     };
-  }, []);
+  }, [onKeyDownEsc]);
 
   return (
-    <div className={s.overlay} onClick={closeModal}>
+    <div className={s.overlay} onClick={() => onKeyDownEsc()}>
       <div className={s.modal}>
         <img src={src} alt={alt} />
       </div>
